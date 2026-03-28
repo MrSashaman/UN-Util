0c2013c352ea50942790b066cd2cb7e010df03aa
psash <psasha2811@gmail.com>
2026-03-28 10:23:45 +0700

    0.1.3

--- UN-Util/Plugin.cs
+++ UN-Util/Plugin.cs
@@ -1,6 +1,8 @@
 ﻿using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.API.Features.Items;
+using Exiled.API.Features.Lockers;
+using Exiled.Events.EventArgs.Map;
 using Exiled.Events.EventArgs.Map;
 using Exiled.Events.EventArgs.Player;
 using Exiled.Events.EventArgs.Scp049;
@@ -11,6 +13,7 @@ using PlayerRoles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 using MapEvents = Exiled.Events.Handlers.Map;
 
@@ -21,13 +24,16 @@ namespace UN_Util
         public override string Name => "UnitedUtil";
         public override string Prefix => "UN_Utils";
         public override string Author => "mrSashaman";
-        public override Version Version => new Version(0, 1, 2);
+        public override Version Version => new Version(0, 1, 3);
 
         private readonly System.Random rnd = new System.Random();
         private Dictionary<Player, CoroutineHandle> choosingPlayers = new Dictionary<Player, CoroutineHandle>();
 
         private Player traitor;
         private string traitorId;
+        private Locker anomalousLocker;
+        private Room anomalousRoom;
+        private readonly System.Random lockerRnd = new System.Random();
         private bool traitorActive = false;
 
         public static Plugin Singleton;
@@ -45,11 +51,14 @@ namespace UN_Util
 
             Exiled.Events.Handlers.Player.Verified += PlayerVerified;
             Exiled.Events.Handlers.Player.Died += OnDead;
+            MapEvents.Generated += OnMapGenerated;
+            Exiled.Events.Handlers.Player.InteractingLocker += OnInteractingLocker;
             Exiled.Events.Handlers.Player.FlippingCoin += ThanksCoin;
             Exiled.Events.Handlers.Player.Hurting += OnHurting;
             Exiled.Events.Handlers.Player.Escaping += OnEscaping;
 
             Exiled.Events.Handlers.Scp049.StartingRecall += OnStartingRecall;
+            Exiled.Events.Handlers.Player.TriggeringTesla += OnTriggeringTesla;
             Exiled.Events.Handlers.Scp096.AddingTarget += OnAddingTarget;
 
             MapEvents.Decontaminating += OnDecontaminating;
@@ -64,6 +73,10 @@ namespace UN_Util
             Exiled.Events.Handlers.Server.WaitingForPlayers -= Hello;
             Exiled.Events.Handlers.Server.RoundEnded -= OnRoundEnded;
             Exiled.Events.Handlers.Server.RoundStarted -= RoundStart;
+            Exiled.Events.Handlers.Player.TriggeringTesla -= OnTriggeringTesla;
+            MapEvents.Generated -= OnMapGenerated;
+            Exiled.Events.Handlers.Player.InteractingLocker -= OnInteractingLocker;
+
             Exiled.Events.Handlers.Server.RespawningTeam -= OnRespawningTeam;
 
             Exiled.Events.Handlers.Player.Verified -= PlayerVerified;
@@ -85,6 +98,81 @@ namespace UN_Util
             Log.Info("****WELCOME*****");
             Log.Info("Dev Discord: https://discord.gg/5pBt7cj8B9 ");
             Log.Info("Version: " + Version);
+        }
+
+
+        private void OnMapGenerated()
+        {
+            var room = Room.List.FirstOrDefault(r => r.Type == RoomType.LczGlassBox);
+            anomalousRoom = Room.List.FirstOrDefault(r => r.Type == RoomType.LczGlassBox);
+            if (room == null)
+            {
+                Log.Warn("GR-18 не найдена!");
+                return;
+            }
+
+            var lockers = Locker.List.Where(l => l.Room == room).ToList();
+
+            if (lockers.Count == 0)
+            {
+                Log.Warn("В GR-18 нет шкафов!");
+                return;
+            }
+
+            anomalousLocker = lockers[lockerRnd.Next(lockers.Count)];
+
+            Log.Info("Аномальный шкаф выбран!");
+        }
+
+
+
+        private void OnInteractingLocker(InteractingLockerEventArgs ev)
+        {
+            if (ev.Player == null || ev.InteractingLocker == null) return;
+
+            if (anomalousLocker == null || ev.InteractingLocker.Base != anomalousLocker.Base)
+                return;
+
+            if (ev.Player.Role.Team == Team.SCPs)
+                return;
+
+            if (anomalousRoom == null) return;
+
+            int roll = lockerRnd.Next(100);
+
+            anomalousRoom.Color = Color.yellow;
+
+            Timing.CallDelayed(3f, () =>
+            {
+                if (anomalousRoom != null)
+                    anomalousRoom.ResetColor();
+            });
+
+            if (roll < 10)
+            {
+                ev.Player.EnableEffect(EffectType.SeveredHands, 10f);
+                ev.Player.ShowHint("Аномалия.. Руки оторваны", 3f);
+            }
+            else if (roll < 20)
+            {
+                ev.Player.EnableEffect(EffectType.Blinded, 5f);
+                ev.Player.ShowHint("Аномалия.. Темнота", 3f);
+            }
+            else if (roll < 30)
+            {
+                ev.Player.EnableEffect(EffectType.Ensnared, 6f);
+                ev.Player.ShowHint("Аномалия.. Ты не можешь двигаться", 3f);
+            }
+            else if (roll < 40)
+            {
+                ev.Player.EnableEffect(EffectType.Poisoned, 8f);
+                ev.Player.ShowHint("Аномалия.. Тебе плохо", 3f);
+            }
+            else if (roll < 50)
+            {
+                ev.Player.EnableEffect(EffectType.Slowness, 10f);
+                ev.Player.ShowHint("Аномалия.. Ты замедлен", 3f);
+            }
         }
 
         private void PlayerVerified(VerifiedEventArgs ev)
@@ -179,6 +267,17 @@ namespace UN_Util
         {
             player.Role.Set(RoleTypeId.ChaosRifleman);
             player.Broadcast(5, "Ты выбрал Chaos!");
+        }
+
+
+        private void OnTriggeringTesla(TriggeringTeslaEventArgs ev)
+        {
+            if (ev.Player == null) return;
+
+            if (ev.Player.Role.Team != Team.SCPs)
+            {
+                ev.IsAllowed = false;
+            }
         }
 
         public void OnDecontaminating(DecontaminatingEventArgs ev)
@@ -281,6 +380,8 @@ namespace UN_Util
             var reward = Config.Coin.Rewards[rnd.Next(Config.Coin.Rewards.Count)];
             player.AddItem(reward);
         }
+
+
 
         private void OnRespawningTeam(RespawningTeamEventArgs ev)
         {
